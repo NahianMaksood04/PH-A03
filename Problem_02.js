1ce99521114077540387340dc8b0d321578116ae
NahianMaksood04 <nahianmaksood04@gmail.com>
2025-08-04 01:22:48 +0600

    problem 2

--- Problem_02.js
+++ Problem_02.js
@@ -1,9 +1,3 @@
-/** Problem -01 ( Divide the Asset ) */
-var area = 800;
-//write your code here
-var individualArea = area / 2;
-console.log(individualArea);
-
 /** Problem -02 ( Cycle or Laptop ) */
 var money = 10000;
 //write your code here
