c6b71d5f3cd7835e034a133a395be7f03990175b
NahianMaksood04 <nahianmaksood04@gmail.com>
2025-08-04 19:37:12 +0600

    done

--- Problem_06.js
+++ Problem_06.js
@@ -2,39 +2,9 @@
 var experience = 30;
 var startingSalary = 45000;
 //write your code here
-
-
-
-var sum = 0;
-for(let i =1; i <=10; i++) {
-    sum = sum + i;
-}console.log(sum)
-
-
-var bonus = 0;
-for (let i = 1; i<=experience; i++){
-    bonus = startingSalary + startingSalary * 5/100;
-}console.log(bonus)
-
-
-var i = 0;
-while (i<=experience) {
-    
-i = startingSalary + startingSalary * 5/100;
-}console.log(i)
-
-
-
-
-
-
-increasedSalary = 45000 + 45000 * 5/100
-final = increasedSalary + increasedSalary *5/100
-final2 = final + final *5/100
-final2
-
-
-
-
-// console.log(finalSalary.toFixed(2));
-149487.4
+for (var i = 1; i <= experience; i++) {
+	var increment = startingSalary * (5 / 100);
+	startingSalary = startingSalary + increment;
+	finalSalary = startingSalary;
+}
+console.log(finalSalary.toFixed(2));
--- final.js
+++ final.js
@@ -41,3 +41,14 @@ var student = { name: "jhanku", roll: 1014, department: "cse" };
 console.log(
 	student.name + student.roll + `.` + student.department + `@ph.ac.bd`
 );
+
+/** Problem 06 :  (Current Salary )  */
+var experience = 30;
+var startingSalary = 45000;
+//write your code here
+for (var i = 1; i <= experience; i++) {
+	var increment = startingSalary * (5 / 100);
+	startingSalary = startingSalary + increment;
+	finalSalary = startingSalary;
+}
+console.log(finalSalary.toFixed(2));
